fc22c96559383055216baa857bac92bb35b15c20
Murendeni Mukwevho <murendenim@girlcode.co.za>
2023-06-22 12:00:28 +0200

    Pull categories for api

--- src/components/CategorySection.jsx
+++ src/components/CategorySection.jsx
@@ -1,39 +1,20 @@
+import { useEffect, useState} from "react"
 import { Link } from "react-router-dom";
-import furnImg from "../assets/furniture.jpg"
 
 const CategorySection = () => {
-    const CatList = [
-        {
-            name: "Furniture",
-            path: "/category/furniture",
-            catImg: furnImg
-        },
-        {
-            name: "Hand Bag",
-            path: "/category/hand-bag",
-            catImg: furnImg
-        },
-        {
-            name: "Books",
-            path: "/category/books",
-            catImg: furnImg
-        },
-        {
-            name: "Tech",
-            path: "/category/tech",
-            catImg: furnImg
-        },
-        {
-            name: "Sneakers",
-            path: "/category/sneaker",
-            catImg: furnImg
-        },
-        {
-            name: "Travel",
-            path: "/category",
-            catImg: furnImg
-        }
-    ]
+    const [catList, setCatList] = useState([])
+
+    useEffect(() => {
+        fetch(`http://127.0.0.1:5000/categories`)
+            .then(resp => resp.json())
+            .then(resp => {
+                setCatList(resp.categories)
+            })
+            .catch(error => {
+                console.log(error)
+            })
+    }, [])
+
 
     return (
         <section className="pt-20">
@@ -42,9 +23,9 @@ const CategorySection = () => {
                     <h2 className="text-2xl font-extrabold">Shop Our Top Categories</h2>
                 </div>
                 <div className="grid grid-cols-6 gap-8">
-                    { CatList.map(cat => (
-                        <Link to={cat.path} className="rounded-xl overflow-hidden relative" key={cat.name}>
-                            <img src={cat.catImg} alt={`${cat.name} Category`}/>
+                    { catList.map(cat => (
+                        <Link to={`/category/${cat.slug}`} className="rounded-xl overflow-hidden relative" key={cat.id}>
+                            <img src={cat.img} alt={`${cat.name} Category`}/>
                             <h3 className="absolute top-3 left-1/2 text-2xl font-extrabold text-girlcode-pink -translate-x-1/2">{cat.name}</h3>
                         </Link>
                     )) }
